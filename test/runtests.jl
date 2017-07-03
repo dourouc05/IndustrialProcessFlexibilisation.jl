b128c1f23ccf53a67703bed5ff93bd5ad15b7aae
Thibaut Cuvelier <dourouc05@gmail.com>
2017-07-03 20:04:08 +0200

    Replace Gurobi by CBC in runtests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Base.Test
 using Base.Dates
 import TimeSeries: TimeArray, timestamp, colnames, values # Avoid name clashing on from
 using JuMP
-using Gurobi
+using Cbc
 using IndustrialProcessFlexibilisation
 
 @testset "IndustrialProcessFlexibilisation.jl" begin

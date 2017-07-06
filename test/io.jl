62446ec5694014bcede081eebc4f1940e61313d8
Thibaut Cuvelier <dourouc05@gmail.com>
2017-07-06 21:50:14 +0200

    Corrected tests for order book conversion>.

--- test/io.jl
+++ test/io.jl
@@ -20,14 +20,16 @@
       # Change timing bounds to get rid of one order.
       t2 = Timing(timeBeginning=date, timeHorizon=Day(5), timeStepDuration=Hour(1), shiftBeginning=date, shiftDuration=Hour(24))
       obm = convert(Array, ob, t2, shiftly=true, restrictToTiming=true)
+      println(obm)
       @test(size(obm, 1) == 5) # 5 days, 1-day shifts.
-      @test(size(obm, 2) == 2) # Products.
-      @test(sum(obm, 1) ≈ [50., 0.]')
+      @test(size(obm, 2) == 1) # Products.
+      @test(sum(obm, 1) ≈ [50.]')
       @test(sum(obm, 2) ≈ [50., 0., 0., 0., 0.])
 
-      # The two previous test cases should not be affected by restrictToTiming.
+      # The two previous test cases should not be affected by restrictToTiming, except for matrix size (some orders are
+      # deleted from the order book before conversion with restrictToTiming, hence some products may be lost).
       @test(convert(Array, ob, t1, shiftly=true, restrictToTiming=true) ≈ convert(Array, ob, t1, shiftly=true, restrictToTiming=false))
-      @test(convert(Array, ob, t2, shiftly=true, restrictToTiming=true) ≈ convert(Array, ob, t2, shiftly=true, restrictToTiming=false))
+      @test(convert(Array, ob, t2, shiftly=true, restrictToTiming=true) ≈ sum(convert(Array, ob, t2, shiftly=true, restrictToTiming=false), 2))
 
       # restrictToTiming has an effect on *past* orders.
       ob = OrderBook(Dict{DateTime, Tuple{Product, Float64}}(timeBeginning(t1) - Week(1) => (p1, 50), timeBeginning(t1) + Week(1) => (p2, 50)))
@@ -40,8 +42,8 @@
 
       obm = convert(Array, ob, t1, shiftly=true, restrictToTiming=true)
       @test(size(obm, 1) == 14) # 2 weeks, 1-day shifts.
-      @test(size(obm, 2) == 2) # Products.
-      @test(sum(obm, 1) ≈ [0., 50.]')
+      @test(size(obm, 2) == 1) # Products.
+      @test(sum(obm, 1) ≈ [50.]')
       @test(sum(obm, 2) ≈ [0., 0., 0., 0., 0., 0., 50., 0., 0., 0., 0., 0., 0., 0.])
     end
   end

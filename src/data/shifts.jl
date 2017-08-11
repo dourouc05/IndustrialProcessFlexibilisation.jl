e97ed12d339e9b735fb4aa6a79fec2939a814fc2
Thibaut Cuvelier <dourouc05@gmail.com>
2017-08-11 19:24:55 +0200

    Start to create the new needed binary variables.

--- src/data/shifts.jl
+++ src/data/shifts.jl
@@ -4,52 +4,50 @@ of the optimisation, as described in a `Timing` object), and the possible durati
 
   * `shiftBeginning`: date at which the first shift begins (for example, `now()` or `DateTime(2016, 12, 15, 6)`);
     it may be before `timeBeginning`, but not after
-  * `shiftDuration`: duration of a worker's shift (for example, `Hour(8)`). It may be either a duration or a vector
-    of durations (if the shift length may vary). 
+  * `shiftDuration`: duration of a worker's shift (for example, `Hour(8)`). It may be either a duration or a range
+    of durations (if the shift length may vary). If values are given as integers, they are considered as hours. 
 """
 struct Shifts
   beginning::DateTime
-  durations::Vector{TimePeriod}
+  durations::StepRange{Hour, Hour}
   
-  function Shifts(beginning::DateTime, durations::Vector{TimePeriod})
+  function Shifts(beginning::DateTime, durations::StepRange{Hour, Hour})
     if length(shiftDurations) == 0
       error("No shift duration given. Please provide at least one.")
     end
 
-    for shiftDuration in durations
-      if typeof(shiftDuration) != Hour
-        error("A shift is not an integer number of hours; please only use Hour(x) for shifts of x hours.")
-      end
-    end
-
     return new(beginning, durations)
-  end
-
-  function Shifts(beginning::DateTime, shiftDuration::TimePeriod)
-    if typeof(shiftDuration) != Hour
-      error("A shift is not an integer number of hours; please only use Hour(x) for shifts of x hours.")
-    end
-    
-    return new(beginning, TimePeriod[shiftDuration])
   end
 end
 
-function Shifts(timing::Timing, beginning::DateTime, durations::Vector{TimePeriod})
+Shifts(beginning::DateTime, durations::StepRange{Int, Int}) = Shifts(beginning, Hour(durations.start) : Hour(durations.step) : Hour(durations.stop))
+Shifts(beginning::DateTime, shiftDuration::Hour) = Shifts(beginning, shiftDuration : Hour(1) : shiftDuration)
+Shifts(beginning::DateTime, shiftDuration::Int) = Shifts(beginning, Hour(shiftDuration) : Hour(1) : Hour(shiftDuration))
+
+function Shifts(timing::Timing, beginning::DateTime, durations)
   if beginning > timeBeginning(timing)
     error("The shifts must begin at the latest at the beginning of the optimisation horizon.")
   end
+
+  return Shifts(beginning, durations)
 end
 
 shiftBeginning(s::Shifts) = s.beginning
-shiftDurations(s::Shifts) = s.durations
-nShiftDurations(s::Shifts) = length(shiftDurations(s))
-shiftDuration(s::Shifts) = if nShiftDurations(s) == 1; s.durations[1]; else; error("There are multiple shift durations. Use the functions shiftDurations and nShiftDurations instead."); end
+shiftDurations(s::Shifts) = collect(s.durations)
+shiftDurationsStart(s::Shifts) = s.durations.start
+shiftDurationsStep(s::Shifts) = s.durations.step
+shiftDurationsStop(s::Shifts) = s.durations.stop
+nShiftDurations(s::Shifts) = length(s.durations)
 
+shiftDuration(s::Shifts) = if nShiftDurations(s) == 1; s.durations[1]; else; error("There are multiple shift durations. Use the functions shiftDurations and nShiftDurations instead."); end
 nTimeStepsPerShift(t::Timing, s::Shifts) = map((sd) -> nTimeSteps(t, sd), shiftDurations(s)) 
 
 """
 Returns the number of shifts from `s` that are contained within the optimisation horizon of `t`. If there are multiple shift
 lengths, this function considers the longest possible length, i.e. the minimum number of shifts. 
+
+Another version of the function uses a third argument that gives the shift duration (instead of taking the maximum one). 
+This duration must pertain to the `s` object. 
 """
 function nShifts(t::Timing, s::Shifts) 
   # Intricacies start when the optimisation and shifts do not start at the same time. If the shifts start sooner,
@@ -68,4 +66,14 @@ function nShifts(t::Timing, s::Shifts)
   sd = maximum(shiftDurations(s))
   return ceil(Int, Dates.toms(timeHorizon(t)) / Dates.toms(sd)) +
     ceil(Int, min(Dates.toms((timeBeginning(t) - shiftBeginning(s))), Dates.toms(sd)) / Dates.toms(sd))
+end
+
+function nShifts(t::Timing, s::Shifts, sl::TimePeriod)
+  allDurations = shiftDurations(s)
+  if sl in allDurations
+    return ceil(Int, Dates.toms(timeHorizon(t)) / Dates.toms(sl)) +
+      ceil(Int, min(Dates.toms((timeBeginning(t) - shiftBeginning(s))), Dates.toms(sl)) / Dates.toms(sl))
+  else
+    error("Given duration $(sl) not found in the durations allowed by the Shifts object. Allowed durations are: $(allDurations)")
+  end
 end
--- src/model/ds/timing.jl
+++ src/model/ds/timing.jl
@@ -1,19 +1,26 @@
 struct TimingModel
   timing::Timing
+  shifts::Shifts
 
-  shiftOpen::Array{JuMP.Variable, 1}
+  shiftOpen::Array{Array{JuMP.Variable, 1}, 1} # For each possible shift, a list of JuMP variables. 
 
-  function TimingModel(m::Model, timing::Timing)
-    shiftOpen = @variable(m, [t=1:nShifts(timing)], Bin)
-    for t in 1:nShifts(timing)
-      setname(shiftOpen[t], "shiftOpen_$(t)")
+  function TimingModel(m::Model, timing::Timing, shifts::Shifts)
+    shiftOpen = Array{JuMP.Variable, 1}[]
+    for shift in shiftDurations(shifts)
+      so = @variable(m, [t=1:nShifts(Timing, shifts, shift)], Bin)
+      for t in 1:nShifts(Timing, shifts, shift)
+        # TODO: ensure the name makes sense... Just a number? Would be much better to have a start date, but must be encoded properly... 
+        setname(shiftOpen[t], "shiftOpen_start$(t)_duration$(shift.value)") # shift is always a Hour() object. 
+      end
+      push!(shiftOpen, so)
     end
 
-    return new(timing, shiftOpen)
+    return new(timing, shifts, shiftOpen)
   end
 end
 
 timing(hr::TimingModel) = hr.timing
+shifts(hr::TimingModel) = hr.shifts
 shiftOpen(hr::TimingModel, s::Int) = hr.shiftOpen[s]
 
 shiftOpen(hr::TimingModel, d::DateTime) = shiftOpen(hr, dateToShift(hr, d))
@@ -25,16 +32,23 @@ timeBeginning(hr::TimingModel) = timeBeginning(timing(hr))
 timeHorizon(hr::TimingModel) = timeHorizon(timing(hr))
 timeEnding(hr::TimingModel) = timeEnding(timing(hr))
 timeStepDuration(hr::TimingModel) = timeStepDuration(timing(hr))
-shiftBeginning(hr::TimingModel) = shiftBeginning(timing(hr))
-shiftDuration(hr::TimingModel) = shiftDuration(timing(hr))
 
 nTimeSteps(hr::TimingModel, d::Period) = nTimeSteps(timing(hr), d)
 nTimeSteps(hr::TimingModel) = nTimeSteps(timing(hr))
 nTimeStepsPerShift(hr::TimingModel) = nTimeStepsPerShift(timing(hr))
-nShifts(hr::TimingModel) = nShifts(timing(hr))
 dateToTimeStep(hr::TimingModel, d::DateTime) = dateToTimeStep(timing(hr), d)
-dateToShift(hr::TimingModel, d::DateTime) = dateToShift(timing(hr), d)
 eachTimeStep(hr::TimingModel; kwargs...) = eachTimeStep(timing(hr); kwargs...)
+
+# Link to the methods of Shifts. 
+shiftBeginning(hr::TimingModel) = shiftBeginning(shifts(hr))
+shiftDurations(hr::TimingModel) = shiftDurations(shifts(hr))
+shiftDurationsStart(s::Shifts) = shiftDurationsStart(shifts(hr))
+shiftDurationsStep(s::Shifts) = shiftDurationsStep(shifts(hr))
+shiftDurationsStop(s::Shifts) = shiftDurationsStop(shifts(hr))
+nShiftDurations(hr::TimingModel) = nShiftDurations(shifts(hr))
+shiftDuration(hr::TimingModel) = shiftDuration(shifts(hr))
+nShifts(hr::TimingModel) = nShifts(shifts(hr))
+dateToShift(hr::TimingModel, d::DateTime) = dateToShift(shifts(hr), d)
 
 
 # Define the constraints.

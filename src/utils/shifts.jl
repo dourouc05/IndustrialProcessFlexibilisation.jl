2e765fa53eee381e960df8be38ecf342c71883f9
Thibaut Cuvelier <dourouc05@gmail.com>
2017-08-22 02:21:44 +0200

    Allow shift agregation with MIP (WIP).

--- src/utils/shifts.jl
+++ src/utils/shifts.jl
@@ -1,7 +1,7 @@
 """
 Agregates small, atomic shifts as longer shifts, based on the allowed shift lengths. 
 """
-function shiftsAgregation(shiftsOpenRaw::Array{Bool, 1}, timing::Timing, shifts::Shifts)
+function shiftsAgregation(shiftsOpenRaw::Array{Bool, 1}, timing::Timing, shifts::Shifts, solver::MathProgBase.AbstractMathProgSolver)
   # First extract long worked periods, not yet dealing with maximum shift duration. 
   shiftsOpenLong = Tuple{DateTime, Hour}[]
 
@@ -31,6 +31,26 @@ function shiftsAgregation(shiftsOpenRaw::Array{Bool, 1}, timing::Timing, shifts:
     if sol[2] <= maximumShiftDurations(shifts) # Shift short enough: accept it as such! 
       push!(shiftsOpen, sol)
     else # Too long: cut it into pieces. 
+      # Have pieces that are as alike to each other as possible, but not too many pieces either. 
+      # Could probably write an algorithm for this, but let's use an optimisation solver (ensured to be available here). 
+      # After all, it's a figth-generation programming language! 
+      m = Model(solver=solver)
+      @variable(m, n[1:nShiftDurations(shifts)] >= 0, Int)
+      @variable(m, average >= 0)
+      @variable(m, slackPlus[1:nShiftDurations(shifts)] >= 0)
+      @variable(m, slackMinus[1:nShiftDurations(shifts)] >= 0)
+      @constraint(m, dot(n, shiftDurations(shifts)) == sol[2])
+      @constraint(m, c[i=1:nShiftDurations(shifts)], n[i] * shiftDurations(shifts)[i].value + slackPlus[i] - slackMinus[i] == average)
+      @objective(m, Min, sum(slackPlus) + sum(slackMinus) + sum(n))
+      solve(m)
+
+      start = sol[1]
+      for i in nShiftDurations(shifts)
+        for repetition in 1:getvalue(n[i])
+          push!(shiftsOpen, (start, shiftDurations(shifts)[i]))
+          start += shiftDurations(shifts)[i]
+        end
+      end
     end
   end
 

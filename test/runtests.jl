91ac46fb33a52dc3dfa9cf8753d49653bc7388aa
Thibaut Cuvelier <dourouc05@gmail.com>
2017-12-31 14:27:39 +0100

    Missing main file for tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,12 +6,28 @@ using Cbc
 using IndustrialProcessFlexibilisation
 
 # Types used for the tests.
-struct ConcreteRoute <: Route end # data.jl > Routes
-struct DummyProductionObjective <: ProductionObjective end # model.jl > Data structures > Objective
+struct ConcreteRoute <: Route end # Data structures > Routes
+struct DummyProductionObjective <: ProductionObjective end # Optimisation models > Data structures > Objective
 
 @testset "IndustrialProcessFlexibilisation.jl" begin
   include("data.jl")
-  include("model.jl")
+  
+  @testset "Optimisation models" begin
+    include("model_ds.jl")
+
+    @testset "Model building blocks (postConstraints)" begin
+      include("model_postconstraints_timing.jl")
+      include("model_postconstraints_equipments.jl")
+      include("model_postconstraints_equipment.jl")
+      include("model_postconstraints_orderbook.jl")
+      include("model_postconstraints_flows.jl")
+      include("model_postconstraints_objective.jl")
+    end
+
+    include("model_prod.jl")
+    include("model_team.jl")
+  end
+
   include("utils.jl")
   include("io.jl")
 end
